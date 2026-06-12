cccbd5d02103394a52fd5235988795f5e0052000
oliviashewmaker1222 <oliviashewmaker@gmail.com>
2026-06-12 12:00:38 -0400

    started it

--- script.js
+++ script.js
@@ -1,5 +1,89 @@
 // Declare variables below to save the different divs of your story.
+let usaButton = document.querySelector(".usa");
+let usaScreen1 = document.querySelector(".usa-screen1");
 
+let mexicoButton = document.querySelector(".mexico");
+let mexicoScreen1 = document.querySelector(".mexico-screen1");
+
+let franceButton = document.querySelector(".france");
+let franceScreen1 = document.querySelector(".france-screen1");
+
+let argentinaButton = document.querySelector(".argentina");
+let argentinaScreen1 = document.querySelector(".argentina-screen1");
+
+let spainButton = document.querySelector(".spain");
+let spainScreen1 = document.querySelector(".spain-screen1");
+
+let canadaButton = document.querySelector(".canada");
+let canadaScreen1 = document.querySelector(".canada-screen1");
+
+let argentinaButton = document.querySelector(".usa");
+let argentinaScreen1 = document.querySelector(".usa-screen1");
+
+let argentinaButton = document.querySelector(".usa");
+let argentinaScreen1 = document.querySelector(".usa-screen1");
+let argentinaButton = document.querySelector(".usa");
+let argentinaScreen1 = document.querySelector(".usa-screen1");
+let argentinaButton = document.querySelector(".usa");
+let argentinaScreen1 = document.querySelector(".usa-screen1");
+let argentinaButton = document.querySelector(".usa");
+let argentinaScreen1 = document.querySelector(".usa-screen1");
+let opening = document.querySelector(".story-opening");
+
+usaButton.addEventListener("click", function(){
+    usaScreen1.style.display = "block";
+    opening.style.display = "none";
+})
+
+mexicoButtonButton.addEventListener("click", function(){
+    mexicoScreen1.style.display = "block";
+    opening.style.display = "none";
+})
+
+franceButton.addEventListener("click", function(){
+    franceScreen1.style.display = "block";
+    opening.style.display = "none";
+})
+
+argentinaButton.addEventListener("click", function(){
+    argentinaScreen1.style.display = "block";
+    opening.style.display = "none";
+})
+
+spainButton.addEventListener("click", function(){
+    spainScreen1.style.display = "block";
+    opening.style.display = "none";
+})
+
+canadaButton.addEventListener("click", function(){
+    canadaScreen1.style.display = "block";
+    opening.style.display = "none";
+})
+
+portugalButton.addEventListener("click", function(){
+    portugalScreen1.style.display = "block";
+    opening.style.display = "none";
+})
+
+englandButton.addEventListener("click", function(){
+    englandScreen1.style.display = "block";
+    opening.style.display = "none";
+})
+
+japanButton.addEventListener("click", function(){
+    japanScreen1.style.display = "block";
+    opening.style.display = "none";
+})
+
+germanyButton.addEventListener("click", function(){
+    gremanyScreen1.style.display = "block";
+    opening.style.display = "none";
+})
+
+africaButton.addEventListener("click", function(){
+    africaScreen1.style.display = "block";
+    opening.style.display = "none";
+})
 
 
 
