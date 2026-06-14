389533ba1cff91dd8af2991012332720b90ef52f
oliviashewmaker1222 <oliviashewmaker@gmail.com>
2026-06-13 20:50:47 -0400

    generak code done

--- script.js
+++ script.js
@@ -20,10 +20,55 @@ let japanButton = document.querySelector(".japan");
 let germanyButton = document.querySelector(".germany");
 
 let teamNamePlaceholder = document.getElementById('team-name');
+
 let opening = document.querySelector(".story-opening");
+
 let transition = document.querySelector(".transition-scene");
+
 let pitchButton = document.querySelector(".pitch");
+
 let option = document.querySelector(".option-one");
+
+let goal = document.querySelector(".shot-screen");
+let pk = document.querySelector(".pk-screen");
+
+let passButton = document.querySelector(".pass");
+let dribbleButton = document.querySelector(".dribble");
+
+let loser = document.querySelector(".loser-scene");
+let win = document.querySelector(".winner-scene");
+
+let topRButton = document.querySelector(".top-r");
+let topLButton = document.querySelector(".top-l");
+let botRButton = document.querySelector(".bot-r");
+let botLButton = document.querySelector(".bot-l");
+
+let topR = document.querySelector(".top-r-scene");
+let topL = document.querySelector(".top-l-scene");
+let botR = document.querySelector(".bot-r-scene");
+let botL = document.querySelector(".bot-l-scene");
+
+let celebrate1Button = document.querySelector(".top-r-scene button");
+let celebrate2Button = document.querySelector(".bot-r-scene button");
+
+let cont1Button = document.querySelector(".top-l-scene button");
+let cont2Button = document.querySelector(".bot-l-scene button");
+
+let topR2Button = document.querySelector(".top-r2");
+let topL2Button = document.querySelector(".top-l2");
+let botR2Button = document.querySelector(".bot-r2");
+let botL2Button = document.querySelector(".bot-l2");
+
+let topR2 = document.querySelector(".top-r-scene2");
+let topL2 = document.querySelector(".top-l-scene2");
+let botR2 = document.querySelector(".bot-r-scene2");
+let botL2 = document.querySelector(".bot-l-scene2");
+
+let celebrate3Button = document.querySelector(".top-l-scene2 button");
+let celebrate4Button = document.querySelector(".bot-l-scene2 button");
+
+let cont3Button = document.querySelector(".top-r-scene2 button");
+let cont4Button = document.querySelector(".bot-r-scene2 button");
 
 usaButton.addEventListener("click", function(){
     teamNamePlaceholder.innerText = "USA";
@@ -88,4 +133,94 @@ germanyButton.addEventListener("click", function(){
 pitchButton.addEventListener("click", function(){
     option.style.display ="block";
     transition.style.display ="none";
+})
+
+passButton.addEventListener("click", function(){
+    goal.style.display = "block";
+    option.style.display = "none";
+})
+
+dribbleButton.addEventListener("click", function(){
+    loser.style.display = "block";
+    option.style.display = "none";
+})
+
+topLButton.addEventListener("click", function(){
+    topL.style.display = "block";
+    goal.style.display = "none";
+})
+
+topRButton.addEventListener("click", function(){
+    topR.style.display = "block";
+    goal.style.display = "none";
+})
+
+botLButton.addEventListener("click", function(){
+    botL.style.display = "block";
+    goal.style.display = "none";
+})
+
+botRButton.addEventListener("click", function(){
+    botR.style.display = "block";
+    goal.style.display = "none";
+})
+
+celebrate1Button.addEventListener("click", function(){
+    win.style.display = "block";
+    topR.style.display = "none";
+})
+
+celebrate2Button.addEventListener("click", function(){
+    win.style.display = "block";
+    botR.style.display = "none";
+})
+
+cont1Button.addEventListener("click", function(){
+    pk.style.display = "block";
+    topL.style.display = "none";
+})
+
+cont2Button.addEventListener("click", function(){
+    pk.style.display = "block";
+    botL.style.display = "none";
+})
+
+topL2Button.addEventListener("click", function(){
+    topL2.style.display = "block";
+    pk.style.display = "none";
+})
+
+topR2Button.addEventListener("click", function(){
+    topR2.style.display = "block";
+    pk.style.display = "none";
+})
+
+botL2Button.addEventListener("click", function(){
+    botL2.style.display = "block";
+    pk.style.display = "none";
+})
+
+botR2Button.addEventListener("click", function(){
+    botR2.style.display = "block";
+    pk.style.display = "none";
+})
+
+celebrate3Button.addEventListener("click", function(){
+    win.style.display = "block";
+    topL2.style.display = "none";
+})
+
+celebrate4Button.addEventListener("click", function(){
+    win.style.display = "block";
+    botL2.style.display = "none";
+})
+
+cont3Button.addEventListener("click", function(){
+    loser.style.display = "block";
+    topR2.style.display = "none";
+})
+
+cont4Button.addEventListener("click", function(){
+    loser.style.display = "block";
+    botR2.style.display = "none";
 })
